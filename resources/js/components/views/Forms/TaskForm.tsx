8594c2fff8a2c144b384a4f565bd94d171d307b1
Perimie <jerimieperio23@gmail.com>
2024-05-10 14:17:49 +0800

    gg

--- resources/js/components/views/Forms/TaskForm.tsx
+++ resources/js/components/views/Forms/TaskForm.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import * as React from 'react'
 
 
 const TaskForm = () =>{
+
+    const [title, setTitle] = React.useState('')
+    const [description, setDescription] = React.useState('')
 
     const handleSubmit = () => {
         
     }
     return (
         <div className='flex flex-col gap-3'>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full " />
-            <textarea className="textarea textarea-bordered min-h-52" placeholder="Description"></textarea>
+            <input 
+            type="text" 
+            placeholder="Type here" 
+            className="input input-bordered w-full"
+            value={title}
+            onChange />
+            <textarea
+             className="textarea textarea-bordered min-h-52"
+             placeholder="Description"
+             value={description}
+             ></textarea>
             <button className="btn btn-accent" onclick={handleSubmit}>Save Task</button>
         </div>
         
